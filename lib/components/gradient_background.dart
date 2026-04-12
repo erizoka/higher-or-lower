940657f232979dc68c4c860d6ed1047be2ff76f9
Erica Esteves <ericaesteves95@gmail.com>
2026-04-12 19:12:45 -0300

    Externalizando o gradiente de fundo em um componente

---
+++ lib/components/gradient_background.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+  const GradientBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment(1.6, -0.4),
+                colors: [
+                  Colors.deepPurple,
+                  Color(0xff510d98),
+                  Color(0xff35157b),
+                  Color(0xff350e8f),
+                ],
+              )
+          ),
+          child: child
+      ),
+    );
+  }
+}
