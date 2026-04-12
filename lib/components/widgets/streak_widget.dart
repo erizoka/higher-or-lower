826999305cfc9eed048c28c505ea3a27e412caab
Erica Esteves <ericaesteves95@gmail.com>
2026-04-12 20:02:37 -0300

    Deixando o widget com tamanho, texto e icone dinâmico

--- lib/components/widgets/streak_widget.dart
+++ lib/components/widgets/streak_widget.dart
@@ -2,38 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class StreakWidget extends StatelessWidget {
-  const StreakWidget({super.key});
+  final bool hasTrophy;
+  final String text;
+  const StreakWidget({super.key, required this.hasTrophy, required this.text});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Container(
-      width: 170,
-      height: 85,
+      width: size.width * 0.35,
       decoration: BoxDecoration(
         color: Colors.white12,
         border: Border.all(color: Colors.white24, width: 1),
         borderRadius: BorderRadius.all(Radius.circular(10))
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(
-            Icons.emoji_events_outlined,
-            color: Colors.yellow.shade700,
-            size: 35
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Melhor Streak', style: GoogleFonts.manrope(
-                  textStyle: TextStyle(fontWeight: FontWeight.w600))
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (hasTrophy)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.emoji_events_outlined,
+                  color: Colors.yellow.shade700,
+                  size: 35
+                ),
               ),
-              Text('7', style: GoogleFonts.roboto(
-                  textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))
-              )
-            ],
-          )
-        ],
+            if (hasTrophy) SizedBox(width: 5),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(text, style: GoogleFonts.manrope(
+                        textStyle: TextStyle(fontWeight: FontWeight.w600))
+                    ),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text('7', style: GoogleFonts.roboto(
+                        textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold))
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
