0fd98df7b62df0aff61372b6b990ed03143dff79
Erica Esteves <ericaesteves95@gmail.com>
2026-04-16 20:14:11 -0300

    Integrando o controller no GamePage

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -7,63 +7,87 @@ import 'package:higher_or_lower/components/guess_button.dart';
 
 import '../components/widgets/streak_widget.dart';
 import '../components/widgets/title_widget.dart';
+import '../controller/cards_controller.dart';
 
 class GamePage extends StatelessWidget {
   const GamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<CardsController>();
+
     return GradientBackground(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsetsGeometry.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TitleWidget(fontSize: 30,),
-              Text('Adivinhe a próxima carta!',
-                style: GoogleFonts.manrope(textStyle: TextStyle(fontSize: 12)),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    StreakWidget(hasTrophy: false, text: 'Streak Atual',),
-                    SizedBox(width: 25,),
-                    StreakWidget(hasTrophy: true, text: 'Melhor',)
-                  ],
-                ),
-              ),
-              CardStackVersus(),
-              SizedBox(height: 30,),
-              GuessButton(isUp: true, onPressed: (){},),
-              GuessButton(isUp: false, onPressed: (){},),
-              SizedBox(height: 30,),
-              ElevatedButton(
-                onPressed: () => Get.back(),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white12,
-                  foregroundColor: Colors.white,
-                  side: const BorderSide(color: Colors.white24, width: 1),
-                  padding: EdgeInsets.all(8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))
+      child: Stack(
+          children: [
+          Padding(
+            padding: const EdgeInsetsGeometry.symmetric(horizontal: 30, vertical: 50),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TitleWidget(fontSize: 30,),
+                  Text('Adivinhe a próxima carta!',
+                    style: GoogleFonts.manrope(textStyle: TextStyle(fontSize: 12)),
                   ),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.settings_backup_restore_rounded),
-                    SizedBox(width: 10,),
-                    Text('Voltar ao Menu')
-                  ],
-                ),
-              )
-            ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30),
+                    child: Obx(() => Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        StreakWidget(
+                          hasTrophy: false,
+                          text: 'Streak Atual',
+                          streak: controller.currentStreak.value
+                        ),
+                        SizedBox(width: 25,),
+                        StreakWidget(
+                          hasTrophy: true,
+                          text: 'Melhor',
+                          streak: controller.bestStreak.value
+                        )
+                      ],
+                    ),
+                  )),
+                  CardStackVersus(),
+                  SizedBox(height: 30,),
+                  GuessButton(
+                    isUp: true,
+                    onPressed: () async { await controller.handleGuess(true);}
+                  ),
+                  GuessButton(
+                    isUp: false,
+                    onPressed: () async { await controller.handleGuess(false);}
+                  ),
+                  SizedBox(height: 30,),
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.resetGame();
+                      Get.back();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white12,
+                      foregroundColor: Colors.white,
+                      side: const BorderSide(color: Colors.white24, width: 1),
+                      padding: EdgeInsets.all(8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.settings_backup_restore_rounded),
+                        SizedBox(width: 10,),
+                        Text('Voltar ao Menu')
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        ]),
     );
   }
 }
